95205fe8fac9cf338bbe0d17f2fa52d3d1040075
Cevad Tokatli <cevadtokatli@hotmail.com>
2024-01-06 09:46:59 +0300

    Update

--- src/picker-input.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class PickerInput extends React.Component {
-    render() {
-        return(
-            <input type="text" spellCheck={false} />
-        )
-    }
-}
--- src/picker-trigger.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class PickerTrigger extends React.Component {
-    render() {
-        return(
-            <i className={`far fa-calendar icon-${this.props.color}`} aria-hidden="true" />
-        )
-    }
-}
